b7f6c1772d85df8cebe27f8634a9dc42d6438ee5
Pramod Sharma <pramod.sharma@snapdeal.com>
2015-09-22 13:27:21 +0530

    pom.xml first checkin for utils very basic

---
+++ src/main/java/com/dmp/util/HTTPUtils.java
@@ -0,0 +1,158 @@
+package com.dmp.util;
+
+/**
+ * Created by pramod.sharma on 22/09/15.
+ */
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.json.simple.JSONAware;
+
+public class HTTPUtils {
+
+    public static String post(String url, String data, Map<String, String> headerMap) {
+        CloseableHttpClient client = null;
+        try {
+            client = HttpClients.createDefault();
+            HttpPost post = new HttpPost(url);
+            if (data == null) {
+                data = "";
+            }
+            HttpEntity entity = new StringEntity(data);
+            post.setEntity(entity);
+            if (headerMap != null && headerMap.size() > 0) {
+                for (Entry<String, String> en : headerMap.entrySet()) {
+                    post.setHeader(en.getKey(), en.getValue());
+                }
+            }
+            HttpResponse httpResponse = client.execute(post);
+            if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                HttpEntity httpEntity = httpResponse.getEntity();
+                InputStream inputStream = httpEntity.getContent();
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+                String line = null;
+                StringBuilder sb = new StringBuilder(1024);
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                    sb.append('\n');
+                }
+                return sb.toString();
+            }
+        } catch (Exception e) {
+
+        } finally {
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+        return null;
+    }
+
+    public static String postData(String url, String data, String contentType) {
+        CloseableHttpClient client = null;
+        try {
+            client = HttpClients.createDefault();
+            HttpPost post = new HttpPost(url);
+            if (data == null) {
+                data = "";
+            }
+            HttpEntity entity = new StringEntity(data);
+            post.setEntity(entity);
+            post.setHeader("Content-Type", contentType);
+            HttpResponse httpResponse = client.execute(post);
+            if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                HttpEntity httpEntity = httpResponse.getEntity();
+                InputStream inputStream = httpEntity.getContent();
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+                String line = null;
+                StringBuilder sb = new StringBuilder(1024);
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                    sb.append('\n');
+                }
+                return sb.toString();
+            }
+        } catch (Exception e) {
+
+        } finally {
+            if (client != null) {
+                try {
+                    client.close();
+                } catch (Exception e) {
+                }
+            }
+        }
+        return null;
+    }
+
+    public static String get(String url) {
+        return get(url, null);
+    }
+
+    public static String get(String url, Map<String, String> headerMap) {
+        try {
+            InputStream inputStream = getDataStream(url, headerMap);
+            if (inputStream == null) {
+                return null;
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            StringBuilder sb = new StringBuilder(1024);
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+                sb.append('\n');
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static InputStream getDataStream(String url) {
+        return getDataStream(url, null);
+    }
+
+    public static InputStream getDataStream(String url, Map<String, String> headerMap) {
+        CloseableHttpClient client = null;
+        try {
+            client = HttpClients.createDefault();
+            HttpGet get = new HttpGet(url);
+            if (headerMap != null && headerMap.size() > 0) {
+                for (Entry<String, String> en : headerMap.entrySet()) {
+                    get.setHeader(en.getKey(), en.getValue());
+                }
+            }
+            HttpResponse httpResponse = client.execute(get);
+            if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                HttpEntity httpEntity = httpResponse.getEntity();
+                InputStream inputStream = httpEntity.getContent();
+                return inputStream;
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public static String postJson(String url, JSONAware jsonData) {
+        String data = "";
+        if (jsonData != null) {
+            data = jsonData.toJSONString();
+        }
+        return postData(url, data, "application/json");
+    }
+
+}
---
+++ src/main/java/com/dmp/util/StringUtils.java
@@ -0,0 +1,87 @@
+package com.dmp.util;
+
+/**
+ * Created by pramod.sharma on 22/09/15.
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class StringUtils {
+
+    public static Map<String, List<String>> getRequestParameters(String urlStr) {
+        Map<String, List<String>> map = new HashMap<>();
+        if (urlStr == null || urlStr.indexOf('?') == -1) {
+            return map;
+        }
+        urlStr = urlStr.substring(urlStr.indexOf('?') + 1);
+        String[] sarr = urlStr.split("\\&");
+        for (String s : sarr) {
+            if (s.indexOf('=') == -1) {
+                continue;
+            }
+            String key = s.substring(0, s.indexOf('='));
+            String val = s.substring(s.indexOf('=') + 1);
+            List<String> list = map.get(key);
+            if (list == null) {
+                list = new ArrayList<>();
+                map.put(key, list);
+            }
+            list.add(val);
+        }
+        return map;
+    }
+
+    public static String getStringParam(Map<String, List<String>> requestParams, String paramName) {
+        if (requestParams == null) {
+            return null;
+        }
+        List<String> list = requestParams.get(paramName);
+        if (list != null && list.size() > 0) {
+            String s = list.get(0);
+            return s;
+        }
+        return null;
+    }
+
+
+
+    public static String getEscapedString(String s, char charTobeEscaped) {
+        if (s == null) {
+            return "";
+        }
+        s = s.replace(charTobeEscaped + "", "\\" + charTobeEscaped);
+        return s;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isEmpty(String str) {
+        return str == null || str.trim().equals("");
+    }
+
+    public static String listToString(List<String> list) {
+        return listToString(list, ",");
+    }
+
+    public static String listToString(List<String> list, String seperator) {
+        StringBuilder sb = new StringBuilder();
+        if (list != null && list.size() > 0) {
+            boolean first = true;
+            for (String s : list) {
+                if (!first) {
+                    sb.append(seperator);
+                }
+                sb.append(s);
+                first = false;
+            }
+        }
+        return sb.toString();
+    }
+
+}
