7cf9425b513b68a7311e32b861ea4757b775afb9
prmdsharma <prmdsharma@gmail.com>
2015-10-07 15:29:30 +0530

    Run time fixes

---
+++ src/main/webapp/tag/otag.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+ 	(function() {
+ 		var em = document.createElement('script'); em.type = 'text/javascript'; em.async = true;
+ 		em.src = ('https:' == document.location.protocol ? 'https://in-ssl' : 'http://in-cdn') + '.opendmp.com/odmp.js';
+ 		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(em, s);
+ 	})();
+ </script>
