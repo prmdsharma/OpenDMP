e4d003766bd4d12216d35fd555585fa4fa2621cd
prmdsharma <prmdsharma@gmail.com>
2015-10-12 13:21:30 +0530

    Changes releted to User finger printing

--- src/main/webapp/tag/opentag.js
+++ src/main/webapp/tag/opentag.js
@@ -1,8 +1,12 @@
+
+
 <script type="text/javascript">
  	(function() {
  		var od = document.createElement('script');
  		od.type = 'text/javascript'; od.async = true;
- 		od.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'opendmp.com/odmp.js';
+ 		var fingerprint = new Fingerprint().get();
+ 		var queryStr = 'pu=' + {{Page URL}} + '&rurl=' + {{Referrer}} + '&uid='
+ 		od.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'opendmp.in/dcs/v1/visit?' + queryStr;
  		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(od, s);
  	})();
  </script>
