44200749ea1bf251d16086d76c1329f62802398d
prmdsharma <prmdsharma@gmail.com>
2015-10-15 10:20:05 +0530

    Java script tag changes

--- src/main/webapp/tag/opentag.js
+++ src/main/webapp/tag/opentag.js
@@ -3,8 +3,9 @@
     (function() {
         var od = document.createElement('script');
         od.type = 'text/javascript'; od.async = true;
-        var queryStr = 'pu=' + {{Page URL}} + '&rurl=' + {{Referrer}} + '&uid=' + fingerprint + '&cid=' + {{Container ID}};
-        queryStr += '&cb=' + {{Random Number}};
+        var ts = new Date().getTime();
+        var queryStr = 'pu=' + {{Page URL}} + '&rurl=' + {{Referrer}} + '&uid=' + cookie + '&cid=' + {{Container ID}};
+        queryStr += '&ts=' + ts;
         pathStr = '/dcs/v1/visit?';
         od.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'opendmp.in' + pathStr + queryStr;
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(od, s);
