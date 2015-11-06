3a88fc1b34d5766e16dd320034ea51fdb11eeae0
prmdsharma <prmdsharma@gmail.com>
2015-11-06 17:17:20 +0530

    Logging bug fixed

--- src/main/webapp/tag-v2/opentag.js
+++ src/main/webapp/tag-v2/opentag.js
@@ -6,8 +6,8 @@
      /* * * DON'T EDIT BELOW THIS LINE * * */
      (function() {
          var odmp = document.createElement('script'); odmp.type = 'text/javascript'; odmp.async = true;
-         odmp.src = '//' + 'opendmp.in/tag/opendmp.js';
-         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+         odmp.src = '//' + 'opendmp.in/tag-v2/opendmp.js';
+         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(odmp);
      })();
  </script>
  </div>
