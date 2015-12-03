729ff2669bf36ae2f6fff49e177417df2515f565
kundan <kundan.01@snapdeal.com>
2015-12-03 17:00:59 +0530

    refactored user cache

--- src/main/java/com/opendmp/service/UserCache.java
+++ src/main/java/com/opendmp/service/UserCache.java
@@ -13,48 +13,54 @@ import com.google.common.cache.Cache;
 
 @Service
 public class UserCache {
-    private Cache<String, AtomicLong> userMinuteCache = CacheBuilder.newBuilder().maximumSize(100000).expireAfterAccess(1, TimeUnit.MINUTES).build();
-    private Cache<String, AtomicLong> userHourCache = CacheBuilder.newBuilder().maximumSize(1000000).expireAfterAccess(1,TimeUnit.HOURS).build();
-    private Cache<String, AtomicLong> userDayCache = CacheBuilder.newBuilder().maximumSize(10000000).expireAfterAccess(1,TimeUnit.DAYS).build();
+	private Cache<String, AtomicLong> userMinuteCache = CacheBuilder
+			.newBuilder().maximumSize(100000)
+			.expireAfterAccess(1, TimeUnit.MINUTES).build();
+	private Cache<String, AtomicLong> userHourCache = CacheBuilder.newBuilder()
+			.maximumSize(1000000).expireAfterAccess(1, TimeUnit.HOURS).build();
+	private Cache<String, AtomicLong> userDayCache = CacheBuilder.newBuilder()
+			.maximumSize(10000000).expireAfterAccess(1, TimeUnit.DAYS).build();
 
-    public void updateUserMinuteCache(String key) {
-        AtomicLong uHits = new AtomicLong(0);
-        userMinuteCache.getIfPresent(uHits);
-        uHits.incrementAndGet();
-        userMinuteCache.put(key,uHits);
-    }
+	public void updateCount(Cache<String, AtomicLong> cache, String key) {
+		AtomicLong uHits = cache.getIfPresent(key);
+		if (uHits == null) {
+			uHits = new AtomicLong(1);
+			cache.put(key, uHits);
+		} else {
+			uHits.incrementAndGet();
+		}
+	}
 
-    public AtomicLong getUseMinuterHits(String key) {
-        AtomicLong uHits = new AtomicLong();
-        userMinuteCache.getIfPresent(uHits);
+	public AtomicLong getCount(Cache<String, AtomicLong> cache, String key) {
+		AtomicLong uHits = cache.getIfPresent(key);
+		if (uHits == null) {
+			return new AtomicLong(0);
+		}
+		return uHits;
+	}
 
-        return uHits;
-    }
-    public void updateUserHourCache(String key) {
-        AtomicLong uHits = new AtomicLong(0);
-        userHourCache.getIfPresent(uHits);
-        uHits.incrementAndGet();
-        userHourCache.put(key, uHits);
-    }
+	public void updateUserMinuteCache(String key) {
+		updateCount(userMinuteCache, key);
+	}
 
-    public AtomicLong getUserHourHits(String key) {
-        AtomicLong uHits = new AtomicLong();
-        userHourCache.getIfPresent(uHits);
+	public AtomicLong getUseMinuterHits(String key) {
+		return getCount(userMinuteCache, key);
+	}
 
-        return uHits;
-    }
+	public void updateUserHourCache(String key) {
+		updateCount(userHourCache, key);
+	}
 
-    public void updateUserDayCache(String key) {
-        AtomicLong uHits = new AtomicLong(0);
-        userDayCache.getIfPresent(uHits);
-        uHits.incrementAndGet();
-        userDayCache.put(key, uHits);
-    }
+	public AtomicLong getUserHourHits(String key) {
 
-    public AtomicLong getUserDayHits(String key) {
-        AtomicLong uHits = new AtomicLong();
-        userDayCache.getIfPresent(uHits);
+		return getCount(userHourCache, key);
+	}
 
-        return uHits;
-    }
+	public void updateUserDayCache(String key) {
+		updateCount(userDayCache, key);
+	}
+
+	public AtomicLong getUserDayHits(String key) {
+		return getCount(userDayCache, key);
+	}
 }
